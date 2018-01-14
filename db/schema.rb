906c3665176713530387aa51d9dda7ad2f4b14ae
Sasha Goldenson <sgoldens@gmail.com>
2018-01-14 14:49:13 -0800

    Move app dir up one level to remove redundant name, remove username/password from defaults in database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180114063213) do
+ActiveRecord::Schema.define(version: 20180114222216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "homes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "searches", force: :cascade do |t|
     t.string "term"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_searches_on_user_id"
   end
@@ -47,4 +40,5 @@ ActiveRecord::Schema.define(version: 20180114063213) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "searches", "users"
 end
