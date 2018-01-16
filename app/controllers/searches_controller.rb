96707ab70ecc6a0e48eccecbb41e6648392452af
Sasha Goldenson <sgoldens@gmail.com>
2018-01-15 22:24:23 -0800

    styles and gflash, clean up routing

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,31 +1,71 @@
 class SearchesController < ApplicationController
   before_action :require_login
+  before_action :set_search, only: [:show, :edit, :update, :destroy]
+
+  def create
+    @search = Search.new(search_params)
+    @search.user_id = current_user.id
+    if @search.save
+      
+      begin
+        @results = Search.new.is_term_taken?(search_params[:term])
+
+        if @results === true
+          gflash :warning => "#{search_params[:term]} is unavailable"
+        elsif @results === false
+          gflash :success => "#{search_params[:term]} is available"
+        end 
+
+      rescue OpenURI::HTTPError
+        gflash :success => "#{search_params[:term]} is available"
+      end
+
+    end
+
+    respond_to do |f|
+      f.html
+      f.js
+    end
+
+    redirect_to :new_search
+      
+  end
+
+  def destroy
+    @search.delete
+    redirect_to @search, notice: "Your search was deleted successful"
+  end
+
+  def edit
+  end
 
   def index
+
+  end
+
+  def new
     @search = Search.new
   end
 
   def show
   end
 
-  def create
-    @search = Search.new({term: search_params, user: current_user})
-    @result = Search.new.is_term_available(search_params)
-    respond_to do |f|
-      if @search.save
-        f.html
-        f.js 
-      else
-        f.html { render 'index' }
-      end
+  def update
+    if @search.update(search_params)
+      redirect_to @search, notice: "Your search was updated successfully"
+    else
+      render :edit
     end
-
   end
 
   private
 
   def search_params
-    params[:search][:term]
+    params.require(:search).permit(:term, :user)
+  end
+
+  def set_search
+    @search = Search.find(params[:id])
   end
 
   def require_login
--- app/models/search.rb
+++ app/models/search.rb
@@ -3,31 +3,21 @@ require 'open-uri'
 class Search < ApplicationRecord
   belongs_to :user
 
-  validates :term, length: { within: 3..20 } 
+  validates_format_of :term, with: /\A(^[\Aa-zA-Z0-9_-]{3,20})\z/i, on: :create
+  validates :user_id, presence: true
 
-  def is_term_available(input)
-    if input.empty?
-      "Please enter a search term"
+  attr_accessor :html, :term
+
+  def is_term_taken?(input)
+    @html = open("https://reddit.com/user/#{input}",
+                "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36").read
+    # BUG: Title tags are case sensitive, so the search term and the title tag should to be transformed to the
+    # same case before checking
+    if @html.include?("<title>#{input} (u/#{input}) - Reddit</title>") || @html.include?("<title>overview for ")
+      true
+    else
+      false
     end
-    begin
-      html = open("https://reddit.com/user/#{input}",
-                  "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36").read
-      if html
-        # BUG: Title tags are case sensitive, so the search term and the title tag should to be transformed to the
-        # same case before checking
-        if html.include?("<title>#{input} (u/#{input}) - Reddit</title>") || html.include?("<title>overview for ")
-          "User name already exists on Reddit"
-        elsif html.include?("<title>reddit.com: page not found</title>")
-          "User name is probably available (but is returning a 404 error)"
-        else
-          "#{input} might not be a valid user name for Reddit.com"
-        end
-      else
-        "Sorry, that search term doesn't seem to be working"
-      end
-    rescue OpenURI::HTTPError => ex
-      "User name is probably available (404 error)"
-    end 
   end
 
 end
