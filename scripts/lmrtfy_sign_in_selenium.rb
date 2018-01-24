10b7334e93b3cc8303f4fb70cff5d09531fb1ba6
Sasha Goldenson <sgoldens@gmail.com>
2018-01-24 15:59:44 -0800

    remove latest tester, sign in latest tester scripts

--- scripts/lmrtfy_sign_in_selenium.rb
+++ scripts/lmrtfy_sign_in_selenium.rb
@@ -1,7 +1,7 @@
 require "selenium-webdriver"
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--no-sandbox')
-# options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument("--set-chrome-log-file File.absolute_path('chrome_log.txt', File.absolute_path)")
 driver = Selenium::WebDriver.for :chrome, options: options
@@ -35,42 +35,41 @@ wait.until { driver.find_element(:id, 'user_email') }
 # Sign in as the primary tester account
 # Fill out the form:
 driver.find_element(:id, 'user_email').send_keys(@tester_primary['email'])
-
 driver.find_element(:id, 'user_password').send_keys(@tester_primary['password'])
-
 driver.find_element(:name, 'commit').click
 
 # Grab the email of the last created account
 wait.until { driver.find_element(:id, 'last_created_account') }
 @tester_latest['email'] = driver.find_element(:id, 'last_created_account').text
 
-# Logging for debug
+# Logging for debugging
 logfile = File.new('console.out', 'w')
 logfile.sync = true
-
-if @tester_latest['email'] != /\A(^tester)\z/i
-  logfile << "#{DateTime.now.strftime('%T%z')} - ERROR - @tester_latest['email']: #{@tester_latest['email']} email does not match tester account syntax, aborting."
-  exit
-elsif @tester_latest['email'] === /\A(^[tester-]\d{1,}@lmrtfy.com)\z/i
-  logfile << "Info: @tester_latest['email']: #{@tester_latest['email']} matches tester account syntax, continuing."
-else
-  logfile << "Error: Unknown regexp matching error for @tester_latest['email'], aborting"
-  exit
-end
-
-# Logging for debug
 logfile << "tester_primary = #{@tester_primary} \n"
 logfile << "tester_latest = #{@tester_latest} \n"
 
 # Confirm the latest account email is a test account
-Selenium::WebDriver::Wait(:timeout => 5)
+tester_regexp = /\A(tester-\d+@lmrtfy.com)\z/i
+now_formatted = DateTime.now.strftime('%T%z')
+tester_latest_email = "@tester_latest['email']: #{@tester_latest['email']}"
+
+if tester_regexp.match?(@tester_latest['email']) === true
+  logfile << "#{now_formatted} - INFO 1: #{tester_latest_email} matches tester account syntax: #{tester_regexp} ... \n continuing \n"
+elsif tester_regexp.match?(@tester_latest['email']) === false
+  logfile << "#{now_formatted} - ERROR 1 - #{tester_latest_email} email does not match tester account syntax: #{tester_regexp} ... \n aborting \n"
+  exit
+end
+
 # Sign-out
+driver.find_element(:id, 'user-sign-out').click
 
 # Click the Sign-in nav button
+driver.find_element(:id, 'user-sign-in').click
 
+# Sign in as the latest tester account
+driver.find_element(:id, 'user_email').send_keys(@tester_latest['email'])
+driver.find_element(:id, 'user_password').send_keys(@tester_latest['password'])
+driver.find_element(:name, 'commit').click
 
-# Email
-
-# Password
-
-# # Submit form
+# Log a successful run
+logfile << "#{now_formatted} - INFO 2: #{tester_latest_email} successfully logged in ... \n exiting \n"
