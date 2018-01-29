67e25619be033f9acf1f3199eb3aad3be852f96e
Sasha Goldenson <sgoldens@gmail.com>
2018-01-29 12:09:21 -0800

    cleanup rspec for devise to-do

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,8 +1,6 @@
 require 'devise'
 
 RSpec.configure do |config|
-  # config.include Devise::TestHelpers, type: :controller
-  # config.include Devise::TestHelpers, type: :view
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :feature
 end
