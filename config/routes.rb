bb0d5d3177e07415124f671ca3ef5ee148254bb8
Sasha Goldenson <sgoldens@gmail.com>
2018-01-29 12:52:23 -0800

    Add home_controller rspec tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :views
   devise_for :controllers
-  # resources :users
+  devise_for :users
   
   get "/home"          => "home#index"
   
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   post "/sign_in_as_new_test_account", to: "tasks#sign_in_as_new_test_account", as: "sign_in_as_new_test_account"
   post "/remove_latest_test_account", to: "tasks#remove_latest_test_account", as: "remove_latest_test_account"
 
+  resources :users
   resources :tasks
   resources :searches
 
